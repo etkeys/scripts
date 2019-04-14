b05ee6449cc4678aa7d03a0a818ef3dbdced1018
E. Keys <e.t.keys@gmail.com>
2019-04-14 14:16:37 -0400

    backupToServer: cron friendly changes
    Setup some things because cron starts off not knowing stuff
    
    - Explicit references to "HOME"
    - Start the ssh agent

--- backupToServer.sh
+++ backupToServer.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+HOME="/home/erik"
+
 ExecuteRsynccmd(){
     rsynccmd="rsync $options $HOME/$1 $remoteDestination"
     eval "$rsynccmd"
 }
 
-pushd '/home/erik'
+pushd "$HOME"
+
+eval `ssh-agent -s`
+ssh-add .ssh/keys_AgFoxte
 
 localdirs=('bin' 'Documents' 'Music' 'Pictures' 'Templates' 'Videos' '.secure' '.ssh' '.themes/backup')
 options='--progress --protect-args -Cauvi -e ssh'
