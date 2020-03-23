e79eaa9d17401ed123a85e0af4765f29e2c30bad
E. Keys <e.t.keys@gmail.com>
2020-03-23 07:45:23 -0400

    terminal-ini-info: add updates-available

--- terminal-init-info.sh
+++ terminal-init-info.sh
@@ -2,5 +2,4 @@
 
 printf "%s - %s (%s %s %s)\n" "$(lsb_release -sd)" "$(lsb_release -sc)" "$(uname -o)" "$(uname -r)" "$(uname -m)"  
 /etc/update-motd.d/50-landscape-sysinfo
-/etc/update-motd.d/90-updates-available
-printf "\n"
+cat /var/lib/update-notifier/updates-available
