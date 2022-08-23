7d5b55e6fac103a42cb125be5ef114f8dc122176
E. Keys <e.t.keys@gmail.com>
2022-08-23 07:03:21 -0400

    Update nextcloud object storage backup
    - Made nextcloud s3 endpoint a required environment variable.
    - Nextcloud s3 endpoint value will be written to "last-sync" file.

--- backup-nextcloud-object-storage.sh
+++ backup-nextcloud-object-storage.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#************************************************************************
+#*************************************************************************
 # Use awscli to sync nextcloud object storage data to a local drive
-#************************************************************************
+#*************************************************************************
 
 set -e
 
-S3_END_POINT="${S3_BACKUP_END_POINT:-https://us-east-1.linodeobjects.com}"
+NEXTCLOUD_S3_ENDPOINT="${NEXTCLOUD_S3_ENDPOINT:?Nextcloud s3 endpoint not set}"
 MOUNT_POINT="${S3_BACKUP_MOUNT_POINT:=/home/erik/nextcloud_backup}"
 
 declare -A roots
-roots[erik]="etkeys-nextcloud"
+roots[erik]="etkeys-nextcloud-erik"
 
 for LOCAL_ROOT in "${!roots[@]}"; do
     DEST="${MOUNT_POINT}/${LOCAL_ROOT}"
@@ -18,11 +18,14 @@ for LOCAL_ROOT in "${!roots[@]}"; do
     [ ! -d "${DEST}" ] && mkdir "${DEST}"
 
     aws s3 sync \
-        "s3://${roots[$LOCAL_ROOT]}" \
+        "s3://${roots[$LOCAL_ROOT]}/" \
         "${DEST}/." \
-        --endpoint="${S3_END_POINT}" \
+        --endpoint="${NEXTCLOUD_S3_ENDPOINT}" \
         --delete
 done
 
-date > "${MOUNT_POINT}/last-sync.txt"
+cat << EOF > "${MOUNT_POINT}/last-sync.txt"
+$(date)
+"${NEXTCLOUD_S3_ENDPOINT}"
+EOF
 
