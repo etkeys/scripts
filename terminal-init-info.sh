7e34269a964b690510a42fe6b33723952707ecf4
E. Keys <e.t.keys@gmail.com>
2023-07-26 19:19:09 -0400

    Update terminal-init-info.sh
    Remove printing landscape messages since there are permission issues and
    I don't want to deal with fixing. Besides, the info wasn't really useful
    after all.

--- terminal-init-info.sh
+++ terminal-init-info.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 printf "%s - %s (%s %s %s)\n" "$(lsb_release -sd)" "$(lsb_release -sc)" "$(uname -o)" "$(uname -r)" "$(uname -m)"  
-/etc/update-motd.d/50-landscape-sysinfo
 cat /var/lib/update-notifier/updates-available
