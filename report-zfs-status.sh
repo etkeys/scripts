163dcf1a23a3fc58fb87072c60e310476a3f1b7a
E. Keys <e.t.keys@gmail.com>
2025-06-08 14:51:28 -0400

    report-zfs-status.sh: update
    Print all snapshots using a separate option to save print space.

--- report-zfs-status.sh
+++ report-zfs-status.sh
@@ -1,13 +1,59 @@
 #!/usr/bin/env bash
 
 set -e
-set -u
 
-zpool status -v &&
-echo "----------------------" &&
-zpool list -v &&
-echo "----------------------" &&
-zfs list &&
-echo "----------------------" &&
-zfs list -t snapshot
+LIST_ALL_SNAPSHOTS=false
 
+print_core_status() {
+    zpool status -v &&
+    echo "----------------------" &&
+    zpool list -v &&
+    echo "----------------------" &&
+    zfs list
+}
+
+print_snapshot_status() {
+    echo "----------------------"
+
+    if [ "$LIST_ALL_SNAPSHOTS" = true ]; then
+        zfs list -t snapshot
+    else
+        zfs list -t snapshot -o name |
+            tail -n+2 |
+            cut -d '@' -f 2 |
+            sort |
+            uniq |
+            while read -r snapshot; do
+                echo "@$snapshot"
+            done
+    fi
+}
+
+print_usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  --snapshots      List all snapshots instead of a summary"
+    echo "  -h, --help       Display this help message and exit"
+}
+
+for arg in "$@"; do
+    case "$arg" in
+        --snapshots)
+            LIST_ALL_SNAPSHOTS=true
+            ;;
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $arg" >&2
+            print_usage >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$LIST_ALL_SNAPSHOTS" = false ]; then
+    print_core_status
+fi
+print_snapshot_status
