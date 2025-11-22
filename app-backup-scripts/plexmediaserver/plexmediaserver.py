8a254bf4a6954b3c29be568a7c49429ad34f77df
E. Keys <e.t.keys@gmail.com>
2025-11-22 11:55:42 -0500

    app backup scripts: add plexmediaserver backup handler

---
+++ app-backup-scripts/plexmediaserver/plexmediaserver.py
@@ -0,0 +1,46 @@
+import subprocess
+from typing import Dict, Any, Tuple
+
+class Handler:
+    def run(self, vars_dict: Dict[str, Any]) -> Tuple[bool, str]:
+        """
+        Process Plex Media Server backup with given variables.
+        
+        Returns a tuple indicating success and a message.
+        """
+        try:
+            backup_dir = vars_dict.get('backup_dir', None)
+            tar_dir = vars_dict.get('tar_dir', None)
+            tar_file = vars_dict.get('tar_file', None)
+
+            # From caller
+            if not backup_dir:
+                return False, "backup_dir variable is missing."
+            if not tar_file:
+                return False, "tar_file variable is missing."
+
+            # From config.yml
+            if not tar_dir:
+                return False, "tar_dir not defined."
+
+            tar_file_full_name = f"{backup_dir}/{tar_file}"
+
+            proc = subprocess.run(
+                ["tar", "-czf", tar_file_full_name,
+                 "--exclude='Plex Media Server/Cache/*",
+                 "--exclude='Plex Media Server/Logs/*",
+                 "--exclude='Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db-shm'",
+                 "--exclude='Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db-wal'",
+                 "--exclude='Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db-journal'",
+                 "--exclude='Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db'",
+                 "-C", tar_dir, "./"],
+                capture_output=True,
+                text=True
+            )
+
+            if proc.returncode != 0:
+                return False, f"Tar command failed: {proc.stderr.strip()}"
+
+            return True, f"Successfully created backup: {tar_file_full_name}."
+        except Exception as e:
+            return False, f"Exception occurred: {str(e)}"
